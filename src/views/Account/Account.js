768c7799693282e899ebfe06844679d4b22786bd
Peng <1a84@proton.me>
2023-01-11 22:46:58 +0100

    fix: always show account

--- src/views/Account/Account.js
+++ src/views/Account/Account.js
@@ -5,47 +5,30 @@ import React, { useEffect } from 'react';
 import AppBar from '../../components/AppBar';
 import ContentWrapper from '../../components/ContentWrapper';
 
-function Home({
-  account,
-  isLoadingAccount,
-  pageContent,
-  onEnter,
-  onLogOut,
-  onDelete,
-}) {
-
+function Home({ account, isLoadingAccount, pageContent, onEnter, onLogOut, onDelete }) {
   useEffect(() => {
     onEnter();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  function renderContent() {
-    if (isLoadingAccount) {
-      return <Spinner />;
-    }
-
-    if (account?.userId) {
-      return (
-        <>
-          <PageHeader title={`Hi ${account.username}`} />
-          <Text>Created at {format(account.createdAt, 'Pp')}</Text>
-          {!!account.telegramId && <Text>Telegram id: {account.telegramId}</Text>}
-
-          <Button label="Log out" onClick={onLogOut} margin="2rem 0 0" />
-          <Button label="Delete account" onClick={onDelete} margin="1rem 0 0" />
-
-          {!!pageContent && <Text>{pageContent}</Text>}
-        </>
-      );
-    }
-
-    return null;
-  }
-
   return (
     <>
       <AppBar title="Account" hasBack />
-      <ContentWrapper>{renderContent()}</ContentWrapper>
+      <ContentWrapper>
+        {isLoadingAccount && <Spinner />}
+        {!!account?.userId && (
+          <>
+            <PageHeader title={`Hi ${account.username}`} />
+            <Text>Created at {format(account.createdAt, 'Pp')}</Text>
+            {!!account.telegramId && <Text>Telegram id: {account.telegramId}</Text>}
+
+            <Button label="Log out" onClick={onLogOut} margin="2rem 0 0" />
+            <Button label="Delete account" onClick={onDelete} margin="1rem 0 0" />
+
+            {!!pageContent && <Text>{pageContent}</Text>}
+          </>
+        )}
+      </ContentWrapper>
     </>
   );
 }
