460c622b059839918a14c3c2b5eae2b93ec5a9b9
Peng <1a84@proton.me>
2023-01-22 21:15:11 +0100

    fix: improve schedule

--- src/views/WatcherDetails/components/WatcherSchedule/WatcherSchedule.js
+++ src/views/WatcherDetails/components/WatcherSchedule/WatcherSchedule.js
@@ -1,7 +1,34 @@
-import { Button, Heading, Text } from 'grommet';
+import { Anchor, Button, Heading, Text } from 'grommet';
 import React from 'react';
 
 import ScheduleSelector from '../ScheduleSelector';
+
+function CurrentSchedule({ schedule, link }) {
+  if (!schedule) {
+    return null;
+  }
+
+  const value = /\(([^)]+)\)/.exec(schedule)?.[1];
+  if (!value) {
+    return value;
+  }
+
+  const parts = value.split(' ');
+  if (parts?.length !== 2) {
+    return null;
+  }
+
+  const num = parts[0];
+  const unit = parts[1];
+
+  return (
+    <Text>
+      This watcher will check <Anchor href={link} label="the page" target="_blank" /> every{' '}
+      {+num === 1 ? '' : `${num} `}
+      {unit}.
+    </Text>
+  );
+}
 
 function WatcherSchedule({ watcher, onDeleteSchedule }) {
   return (
@@ -11,7 +38,7 @@ function WatcherSchedule({ watcher, onDeleteSchedule }) {
       </Heading>
       {!!watcher?.event?.schedule && (
         <>
-          <Text>{watcher.event.schedule}</Text>
+          <CurrentSchedule schedule={watcher.event.schedule} link={watcher.link} />
           <Button label="Delete schedule" onClick={() => onDeleteSchedule(watcher.sortKey)} />
         </>
       )}
