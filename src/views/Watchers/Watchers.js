7f2460b90504689c7fbe9cdf3389d76ec52f676d
Peng <1a84@proton.me>
2023-01-30 21:41:16 +0100

    fix: move refresh

--- src/views/Watchers/Watchers.js
+++ src/views/Watchers/Watchers.js
@@ -1,4 +1,4 @@
-import { Anchor, Box, Button, Spinner, Text } from 'grommet';
+import { Anchor, Box, Spinner, Text } from 'grommet';
 import { Refresh } from 'grommet-icons';
 import React, { useEffect, useState } from 'react';
 
@@ -33,29 +33,20 @@ function Watchers({ watchers, isLoading, isChecking, onFetch, onCheckWatcher }) 
         {watchers.map(watcher => (
           <Box key={watcher.sid} margin="0 0 2rem">
             <HorizontalCenter margin="1rem 0 0">
-              <RouteLink to={`/w/${watcher.sid}`} label={watcher.title} />
-              {watcher.isNew && (
-                <Text color="status-critical" size="small" margin="0 0 0 1rem">
-                  NEW
-                </Text>
-              )}
+              <Text>
+                <RouteLink to={`/w/${watcher.sid}`} label={watcher.title} />
+                {watcher.isNew && (
+                  <Text color="status-critical" size="small" margin="0 0 0 1rem">
+                    NEW
+                  </Text>
+                )}
+              </Text>
             </HorizontalCenter>
             <HorizontalCenter>
               {!!watcher.gotValueAt && (
-                <Text size="xsmall">{formatDateTime(watcher.gotValueAt)}</Text>
-              )}
-
-              {isChecking && checkId === watcher.sid ? (
-                <Spinner size="20px" />
-              ) : (
-                <Button
-                  icon={<Refresh size="20px" />}
-                  onClick={() => {
-                    setCheckId(watcher.sid);
-                    onCheckWatcher(watcher.sid);
-                  }}
-                  hoverIndicator
-                />
+                <Text size="xsmall" margin="0 0.5rem 0 0">
+                  {formatDateTime(watcher.gotValueAt)}
+                </Text>
               )}
             </HorizontalCenter>
 
@@ -63,6 +54,19 @@ function Watchers({ watchers, isLoading, isChecking, onFetch, onCheckWatcher }) 
               <Anchor label={watcher.content} href={watcher.contentLink} target="_blank" />
             ) : (
               <Text>{watcher.content}</Text>
+            )}
+
+            {isChecking && checkId === watcher.sid ? (
+              <Spinner size="xsmall" />
+            ) : (
+              <Refresh
+                size="18px"
+                onClick={() => {
+                  setCheckId(watcher.sid);
+                  onCheckWatcher(watcher.sid);
+                }}
+                hoverIndicator
+              />
             )}
           </Box>
         ))}
