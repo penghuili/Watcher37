a1946474bcad101940cc4c24cddb4227b4d95ff6
Peng <1a84@proton.me>
2023-02-11 09:43:29 +0100

    feat: update pitch

--- src/components/Pitch.js
+++ src/components/Pitch.js
@@ -2,7 +2,7 @@ import { Text } from 'grommet';
 import React from 'react';
 
 function Pitch() {
-  return <Text>PageWatcher checks page updates for you, encrypted.</Text>;
+  return <Text>Create newsletter for any website, encrypted.</Text>;
 }
 
 export default Pitch;
