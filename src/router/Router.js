bcd51c9c01003cb379616af857854d01969a7c07
Peng <peng37@tutanota.com>
2024-01-07 12:40:58 +0100

    remove test code

--- src/router/Router.js
+++ src/router/Router.js
@@ -29,7 +29,7 @@ function Router({ isCheckingRefreshToken, isLoggedIn, isLoadingSettings, isExpir
   }
 
   if (isLoggedIn) {
-    if (!isExpired) {
+    if (isExpired) {
       return (
         <Switch>
           <Route path="/" component={Tickets} />
